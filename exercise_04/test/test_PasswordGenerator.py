74374e8346430a3cd4c63a0578b5365de6a1bcd7
Dídac García <didaquis@gmail.com>
2020-05-16 18:16:58 +0200

    Added assertion

--- exercise_04/test/test_PasswordGenerator.py
+++ exercise_04/test/test_PasswordGenerator.py
@@ -7,9 +7,10 @@ class TestPasswordGenerator(unittest.TestCase):
 	def test_should_be_callable(self):
 		self.assertTrue(callable(PasswordGenerator), 'Should be callable')
 
-	def test_should_return_a_string(self):
+	def test_should_return_a_string_enough_length(self):
 		generator = PasswordGenerator()
 		password = generator.new_password()
+		self.assertTrue(isinstance(password, str), 'Should be a string')
 		self.assertGreaterEqual(len(password), 8, 'Should have at least 8 characters')
 
 if __name__ == '__main__':
