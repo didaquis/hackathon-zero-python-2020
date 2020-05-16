a7b1e9fb8543128a4339b4b5600656c50af86045
Dídac García <didaquis@gmail.com>
2020-05-16 09:58:59 +0200

    Added some content

--- unittest/sum.test.py
+++ unittest/sum.test.py
@@ -1,10 +1,10 @@
 import unittest
 
-from sum import sum
+from sum import sum # from <name_of_file> import <function>
 
 class TestSum(unittest.TestCase):
 
-	def test_sum_should_be_callable(self):
+	def test_sum_should_be_callable(self): # Name of test should be unique
 		self.assertTrue(callable(sum), "Should be callable")
 
 	def test_sum(self):
